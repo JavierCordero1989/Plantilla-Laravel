953e23cab7625b33d376316ace821db24bbfa37b
JavierCL <javicordero20022016@gmail.com>
2018-07-30 15:24:01 -0600

    Controlador
    Se modifico el controlador para exportar/importar archivos excel

--- app/Http/Controllers/ExportImportExcelController.php
+++ app/Http/Controllers/ExportImportExcelController.php
@@ -45,37 +45,55 @@ class ExportImportExcelController extends Controller
         return redirect(route('ruta_a_dirigir'));
     }// Fin de la funcion exportar_a_excel
 
+    public function guardar_a_base_de_datos(Request $request) {
+        $archivo = $request->file('archivo_nuevo');
+        /** El método load permite cargar el archivo definido como primer parámetro */
+        Excel::load($archivo, function ($reader) {
+            /**
+             * $reader->get() nos permite obtener todas las filas de nuestro archivo
+             */
+            foreach ($reader->get() as $key => $row) {
+                /** campos del archivo excel */
+                $data = [
+                    //'campo en base de datos' => 'columna de excel'
+                    'nombre' => $row['nombre'],
+                ];
+
+                /** Una vez obtenido los datos de la fila procedemos a registrarlos */
+                if(!empty($data)){
+                    DB::table('nombre de la tabla')->insert($data);
+                }
+            }
+        });
+    }
+
     public function importar_desde_excel(Request $request) {
-        sleep(10); //Para dormir 10 segundos y probar el modal
-        dd($request->all());
+
+        return response()->json('Ha llegado al controlador');
         /** Si viene un archivo en el request
          * 'archivo_nuevo' => es el nombre del campo que tiene el formulario
          * en la página html.
          */
         if($request->hasFile('archivo_nuevo')) {
-            $archivo = $request->file('archivo_nuevo');
-            /** El método load permite cargar el archivo definido como primer parámetro */
-            Excel::load($archivo, function ($reader) {
-                /**
-                 * $reader->get() nos permite obtener todas las filas de nuestro archivo
-                 */
-                foreach ($reader->get() as $key => $row) {
-                    /** campos del archivo excel */
-                    $data = [
-                        //'campo en base de datos' => 'columna de excel'
-                        'nombre' => $row['nombre'],
-                    ];
+            if($request->ajax()){
+                guardar_a_base_de_datos($request);
+                return response()->json('Se han guardado los registros en la base de datos');
+            }
+            else {
+                guardar_a_base_de_datos($request);
 
-                    /** Una vez obtenido los datos de la fila procedemos a registrarlos */
-                    if(!empty($data)){
-                        DB::table('nombre de la tabla')->insert($data);
-                    }
-                }
-            });
+                Flash::sucess('El archivo se ha guardado correctamente en la Base de Datos');
+                return redirect(route('ruta a dirigir'));
+            }
         }
         else {
-            Flash::error('No se ha podido subir el archivo.');
-            return view('vista a dirigir')/*redirect(route('importFile'))*/;
+            if($request->ajax()) {
+                return response()->json('No ha enviado un archivo');
+            }
+            else {
+                Flash::sucess('No ha enviado un archivo');
+                return redirect(route('ruta a dirigir'));
+            }
         }
     }// Fin de la funcion importar_desde_excel
 }
