9ecdaa9d3049a602e3b35c7e6c9d83a61f54369e
JavierCL <javicordero20022016@gmail.com>
2018-08-10 00:23:32 -0600

    Guard Name
    Campo del formulario de creacion de roles, de guard_name, oculto pero con el dato necesario para almacenarlo en la BD.

--- resources/views/roles/fields.blade.php
+++ resources/views/roles/fields.blade.php
@@ -6,8 +6,7 @@
 
 <!-- Identificacion Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('guard_name', 'Guard Name:') !!}
-    {!! Form::text('guard_name', null, ['class' => 'form-control']) !!}
+    {!! Form::hidden('guard_name', 'web') !!}
 </div>
 
 <!-- Submit Field -->
